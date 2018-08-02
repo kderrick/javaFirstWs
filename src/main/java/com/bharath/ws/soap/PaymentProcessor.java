43fa00451bc0eacbb899e2f311ff5602e8c7132c
Kyle <kderrick1988@gmail.com>
2018-08-01 19:25:02 -0700

    update PaymentProcessor interface to use JAX-WS endpoint annotations

--- src/main/java/com/bharath/ws/soap/PaymentProcessor.java
+++ src/main/java/com/bharath/ws/soap/PaymentProcessor.java
@@ -1,9 +1,18 @@
 package com.bharath.ws.soap;
 
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebResult;
+import javax.jws.WebService;
+
 import com.bharath.ws.soap.dto.PaymentProcessorRequest;
 import com.bharath.ws.soap.dto.PaymentProcessorResponse;
 
-public interface PaymentProcessor {
 
-	public PaymentProcessorResponse processPayment(PaymentProcessorRequest paymentProcessorRequest);
+@WebService(name="PaymentProcessor")
+public interface PaymentProcessor {
+	
+	@WebMethod
+	public @WebResult(name="response")PaymentProcessorResponse processPayment
+		(@WebParam(name="aymentProcessorRequest")PaymentProcessorRequest paymentProcessorRequest);
 }

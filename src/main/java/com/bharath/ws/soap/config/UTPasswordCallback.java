b9f45f009a31d4ee620bc20eb926312bd5417f78
Kyle <kderrick1988@gmail.com>
2018-08-06 16:40:19 -0700

    add interceptor properties to WebServiceConfig

---
+++ src/main/java/com/bharath/ws/soap/config/UTPasswordCallback.java
@@ -0,0 +1,17 @@
+package com.bharath.ws.soap.config;
+
+import java.io.IOException;
+
+import javax.security.auth.callback.Callback;
+import javax.security.auth.callback.CallbackHandler;
+import javax.security.auth.callback.UnsupportedCallbackException;
+
+public class UTPasswordCallback implements CallbackHandler {
+
+	@Override
+	public void handle(Callback[] callbacks) throws IOException, UnsupportedCallbackException {
+		// TODO Auto-generated method stub
+
+	}
+
+}

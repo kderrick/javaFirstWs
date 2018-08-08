0b728d2070889d8721c6b58e9f0b6fd42384290a
Kyle <kderrick1988@gmail.com>
2018-08-08 16:49:32 -0700

    implement handle method in UTPasswordCallback

--- src/main/java/com/bharath/ws/soap/config/UTPasswordCallback.java
+++ src/main/java/com/bharath/ws/soap/config/UTPasswordCallback.java
@@ -8,6 +8,8 @@ import javax.security.auth.callback.Callback;
 import javax.security.auth.callback.CallbackHandler;
 import javax.security.auth.callback.UnsupportedCallbackException;
 
+import org.apache.wss4j.common.ext.WSPasswordCallback;
+
 public class UTPasswordCallback implements CallbackHandler {
 
 	private Map<String, String> passwords = new HashMap<>(); 
@@ -20,7 +22,15 @@ public class UTPasswordCallback implements CallbackHandler {
 	@Override
 	public void handle(Callback[] callbacks) throws IOException, UnsupportedCallbackException {
 		
-		
+		for (Callback callback : callbacks) {
+			WSPasswordCallback passwordCallBack = (WSPasswordCallback) callback;
+			String password = passwords.get(passwordCallBack.getIdentifier());
+			
+			if(password!=null) {
+				passwordCallBack.setPassword(password);
+				return;
+			}
+		}
 
 	}
 
